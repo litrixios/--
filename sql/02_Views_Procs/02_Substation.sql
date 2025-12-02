e52335cccce7f26c656947fe0bc93b07174be68a
lth1949 <2839681623@qq.com>
2025-12-02 13:06:45 +0800

    修改回路异常视图

--- sql/02_Views_Procs/02_Substation.sql
+++ sql/02_Views_Procs/02_Substation.sql
@@ -11,7 +11,11 @@ SELECT
     cm.CurrentA,
     CASE
         WHEN cm.VoltageKV IS NULL OR cm.CurrentA IS NULL THEN N'数据不完整'
-        WHEN s.VoltageLevel = '35KV' AND cm.VoltageKV > 37 THEN N'电压越限'
+        WHEN s.VoltageLevel = '35KV' AND cm.VoltageKV > 37 THEN N'电压越上限'
+        WHEN s.VoltageLevel = '35KV' AND cm.VoltageKV < 33 THEN N'电压越下限'
+        WHEN s.VoltageLevel = '0.4KV' AND cm.VoltageKV > 0.428 THEN N'电压越上限'
+        WHEN s.VoltageLevel = '0.4KV' AND cm.VoltageKV < 0.372 THEN N'电压越下限'
+        WHEN cm.CurrentA > 700 THEN N'电流越限'
         ELSE N'正常'
     END AS AbnormalReason
 FROM CircuitMeasurement cm
@@ -19,8 +23,9 @@ JOIN Circuit c ON cm.CircuitId = c.CircuitId
 JOIN Substation s ON cm.SubstationId = s.SubstationId
 WHERE
     (cm.VoltageKV IS NULL OR cm.CurrentA IS NULL)
-    OR (s.VoltageLevel = '35KV' AND cm.VoltageKV > 37);
-
+    OR (s.VoltageLevel = '35KV' AND (cm.VoltageKV > 37 OR cm.VoltageKV < 33))
+    OR (s.VoltageLevel = '0.4KV' AND (cm.VoltageKV > 0.428 OR cm.VoltageKV < 0.372))
+    OR (cm.CurrentA > 700);
 GO
 --变压器异常视图
 CREATE VIEW V_Transformer_AbnormalData
