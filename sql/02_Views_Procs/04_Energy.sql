2a26b619c9a91949849be13d34c79fab3490f82e
lovewaiwai <2662775049@qq.com>
2025-12-02 14:15:05 +0800

    修改了视图，适配能源管理员

--- sql/02_Views_Procs/04_Energy.sql
+++ sql/02_Views_Procs/04_Energy.sql
@@ -10,3 +10,22 @@ FROM EnergyMeasurement e
 JOIN EnergyMeter m ON e.MeterId = m.MeterId
 JOIN FactoryArea fa ON e.FactoryId = fa.FactoryId
 GROUP BY fa.FactoryId, fa.Name, m.EnergyType, CAST(e.CollectTime AS DATE);
+GO
+
+--用于能源管理员的视图
+
+CREATE VIEW V_Energy_Report AS
+SELECT
+    m.DataId,
+    f.Name AS AreaName,      -- 改回英文
+    em.MeterCode AS MeterCode,
+    em.EnergyType AS EnergyType,
+    m.CollectTime AS CollectTime,   -- 改回英文
+    m.Value AS Value,
+    m.Unit AS Unit,
+    m.DataQuality AS Quality,
+    m.NeedVerify AS NeedVerify
+FROM EnergyMeasurement m
+JOIN EnergyMeter em ON m.MeterId = em.MeterId
+JOIN FactoryArea f ON m.FactoryId = f.FactoryId;
+GO
